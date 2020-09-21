99b8f76c397c7524fbfb7323d6f0ed53674f1aac
xuchaoji <admin@xuchaoji.com>
2020-09-21 23:24:14 +0800

    [20200921]最长有效括号 [related]动态规划，还需要再加深对动态规划类问题的分析

---
+++ src/com/xuchaoji/leetcode/sep/LongestValidParentheses.java
@@ -0,0 +1,47 @@
+package com.xuchaoji.leetcode.sep;
+
+import org.junit.Test;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+ *
+ * 示例 1:
+ *
+ * 输入: "(()"
+ * 输出: 2
+ * 解释: 最长有效括号子串为 "()"
+ * 示例 2:
+ *
+ * 输入: ")()())"
+ * 输出: 4
+ * 解释: 最长有效括号子串为 "()()"
+ */
+public class LongestValidParentheses {
+
+    @Test
+    public void test() {
+        String input = "()()()((()))(";
+        System.out.println(longestValidParentheses(input));
+    }
+
+    // https://leetcode-cn.com/problems/longest-valid-parentheses/solution/zui-chang-you-xiao-gua-hao-by-leetcode-solution/
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        int dp[] = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                maxans = Math.max(maxans, dp[i]);
+            }
+        }
+        return maxans;
+    }
+}
