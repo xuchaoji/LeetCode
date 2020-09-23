f4573daf6a84e7a5e93b932f94e48fee99c04466
xuchaoji <admin@xuchaoji.com>
2020-09-23 22:27:57 +0800

    [20200923]斐波那契数列，爬楼梯 [related]数学归纳

---
+++ src/com/xuchaoji/leetcode/sep/ClimbStairs.java
@@ -0,0 +1,31 @@
+package com.xuchaoji.leetcode.sep;
+
+/**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ * 注意：给定 n 是一个正整数。
+ *
+ * 示例 1：
+ *
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+
+ */
+public class ClimbStairs {
+
+    // f(i) = f(i-1) + f(i-2) 斐波那契数列。
+    public int climbStairs(int n) {
+        int p = 0, q = 0, r = 1;
+        for (int i = 1; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+    }
+}
