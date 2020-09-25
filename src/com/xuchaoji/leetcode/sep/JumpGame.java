458ff2eff31f65bf9f31e958750d0966188337c9
xuchaoji <admin@xuchaoji.com>
2020-09-25 23:37:06 +0800

    [20200925]跳跃游戏 [related]

---
+++ src/com/xuchaoji/leetcode/sep/JumpGame.java
@@ -0,0 +1,36 @@
+package com.xuchaoji.leetcode.sep;
+
+/**
+ *给定一个非负整数数组，你最初位于数组的第一个位置。
+ *
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ *
+ * 判断你是否能够到达最后一个位置。
+ *
+ * 示例1:
+ *
+ * 输入: [2,3,1,1,4]
+ * 输出: true
+ * 解释: 我们可以先跳 1 步，从位置 0 到达 位置 1, 然后再从位置 1 跳 3 步到达最后一个位置。
+
+ */
+public class JumpGame {
+
+
+    // num[i] 最远可到达num[i] + i;
+    // 从初始位置开始，以初始位置可到达的最远距离为区间
+    // 遍历区间中最远的距离，更新最远距离，当最远距离超过数组长度，返回true
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int rightmost = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i <= rightmost) {
+                rightmost = Math.max(rightmost, i + nums[i]);
+                if (rightmost >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
