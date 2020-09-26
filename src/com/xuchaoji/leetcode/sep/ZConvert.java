21af42e746c56b9828101ca29eea3a78f11a7ec2
xuchaoji <admin@xuchaoji.com>
2020-09-26 17:47:37 +0800

    [20200925]Z变换 [related]StringBuilder

---
+++ src/com/xuchaoji/leetcode/sep/ZConvert.java
@@ -0,0 +1,42 @@
+package com.xuchaoji.leetcode.sep;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+ *
+ * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+ *
+ * L   C   I   R
+ * E T O E S I I G
+ * E   D   H   N
+ * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+ */
+public class ZConvert {
+
+    @Test
+    public void test() {
+        String input = "ABCDEFGHIJKLMN";
+        System.out.println(convert(input, 4));
+    }
+
+    public String convert(String s, int numRows) {
+        // numRows个StringBuilder
+        if(numRows < 2) return s;
+        List<StringBuilder> rows = new ArrayList<StringBuilder>();
+        for(int i = 0; i < numRows; i++) rows.add(new StringBuilder());
+        int i = 0, flag = -1;
+        for(char c : s.toCharArray()) {
+            rows.get(i).append(c);
+            // i=0对应第一行，i=numRows -1 对应最后一行，此时反转方向
+            if(i == 0 || i == numRows -1) flag = - flag;
+            i += flag;
+        }
+        StringBuilder res = new StringBuilder();
+        for(StringBuilder row : rows) res.append(row);
+        return res.toString();
+    }
+}
