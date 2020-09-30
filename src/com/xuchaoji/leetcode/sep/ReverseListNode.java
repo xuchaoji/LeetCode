7cd271cf38a7c77d54d92f2e2a0f7d93367fc1d7
xuchaoji <admin@xuchaoji.com>
2020-09-30 23:59:47 +0800

    [20200929]翻转链表 [related]链表

---
+++ src/com/xuchaoji/leetcode/sep/ReverseListNode.java
@@ -0,0 +1,33 @@
+package com.xuchaoji.leetcode.sep;
+
+/**
+ * 反转一个单链表。
+ *
+ * 示例:
+ *
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ */
+public class ReverseListNode {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    public class ListNode {
+        int var;
+        ListNode next;
+        ListNode(int x) {
+            this.var = x;
+        }
+    }
+
+}
