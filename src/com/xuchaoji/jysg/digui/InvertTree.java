fe9aa46a45c5d0a677fc336cc09ee0d8cd8457d4
xuchaoji <admin@xuchaoji.com>
2021-01-03 12:02:26 +0800

    [20200103]反转二叉树  递归

---
+++ src/com/xuchaoji/jysg/digui/InvertTree.java
@@ -0,0 +1,48 @@
+package com.xuchaoji.jysg.digui;
+
+/**
+ * 翻转一棵二叉树。
+ *
+ * 示例：
+ *
+ * 输入：
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 输出：
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ *
+ */
+public class InvertTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (null == root) {
+            return null;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+}
