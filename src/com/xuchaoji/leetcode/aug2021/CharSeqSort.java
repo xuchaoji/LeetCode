ebf02484404b2d9ff2653b97cae52f89901e9b88
xuchaoji <admin@xuchaoji.com>
2021-08-02 22:48:38 +0800

    [20210802]数组排列

---
+++ src/com/xuchaoji/leetcode/aug2021/CharSeqSort.java
@@ -0,0 +1,34 @@
+package com.xuchaoji.leetcode.aug2021;
+
+import org.junit.Test;
+
+/**
+ * 给出集合 [1,2,3,...,n]，其所有元素共有 n! 种排列。
+ *
+ * 按大小顺序列出所有排列情况，并一一标记，当 n = 3 时, 所有排列如下：
+ *
+ * "123"
+ * "132"
+ * "213"
+ * "231"
+ * "312"
+ * "321"
+ * 给定 n 和 k，返回第 k 个排列。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/permutation-sequence
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class CharSeqSort {
+    public String getPermutation(int n, int k) {
+        String res = null;
+
+        return res;
+    }
+
+    @Test
+    public void test() {
+        
+    }
+}
